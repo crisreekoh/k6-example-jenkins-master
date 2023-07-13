49a2540d90b894988bb938bc60cbeb6a07351c72
Cris Carlo Dapitanon <criscarlo.dapitanon@legalmatch.com>
2023-07-13 11:35:15 +0800

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-bat apt-get update
-bat apt-get install dirmngr --install-recommends
-bat apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-bat "deb https://dl.bintray.com/loadimpact/deb stable main" | bat tee -a /etc/apt/sources.list
-bat apt-get update
-bat apt-get install k6
+apt-get update
+apt-get install dirmngr --install-recommends
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+"deb https://dl.bintray.com/loadimpact/deb stable main" | bat tee -a /etc/apt/sources.list
+apt-get update
+apt-get install k6

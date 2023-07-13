1ab3909bd5c8515a3c1a2edc3b7ee7001b986c55
Cris Carlo Dapitanon <criscarlo.dapitanon@legalmatch.com>
2023-07-13 11:55:12 +0800

    Update jenkinsfile

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-runas apt-get update
-runas apt-get install dirmngr --install-recommends
-runas apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
-echo "deb https://dl.k6.io/deb stable main" | runas tee /etc/apt/sources.list.d/k6.list
-runas apt-get update
-runas apt-get install k6
+apt-get update
+apt-get install dirmngr --install-recommends
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb https://dl.k6.io/deb stable main" | tee /etc/apt/sources.list.d/k6.list
+apt-get update
+apt-get install k6
